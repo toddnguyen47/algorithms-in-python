e22c5ee09e30b556b947a5ad6805e52c1485c850
toddnguyen47 <todd.nguyen47@gmail.com>
2018-10-20 12:23:15 -0700

    Delete HTML whitespace using Python

---
+++ delete_whitespace.py
@@ -0,0 +1,36 @@
+# Author: Todd Nguyen
+# Last Updated: Oct 20, 2018 - 12:18
+
+import re
+import os.path
+
+input_filename = input("Enter filename: ")
+
+with open("index.html", "r") as file:
+    data = file.read()
+
+# Use positive look-ahead and positive look-behind to look for whitespace
+pattern = "\s*(?=\<)|(?<=\>)\s*"
+data = re.sub(pattern, "", data)
+print(data)
+print("")
+
+produce_output = input("Would you like to export? (Y/N): ")
+
+if produce_output.lower() == "y":
+    output_filename = input("Enter output filename: ")
+    if os.path.isfile(output_filename):
+        # Make a copy
+        with open(output_filename, "r") as file:
+            backup = file.read()
+        backup_filename = "".join((output_filename, ".bak"))
+        with open(backup_filename, "w") as file:
+            file.write(backup)
+
+    with open(output_filename, "w") as file:
+        file.write(data)
+    
+    print("Finished exporting.")
+
+print("Thank you for using this program!")
+
